43411ed20833d8411a1118359d03417c2911612b
Matt <59450879+mattppal@users.noreply.github.com>
2023-04-14 08:38:52 -0400

    about

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,41 +12,50 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: SITE.website,
-    integrations: [tailwind({
-        config: {
-            applyBaseStyles: false
-        }
-    }), partytown({
-        // Adds dataLayer.push as a forwarding-event.
-        config: {
-            forward: ["dataLayer.push"],
-        }
-    }), react(), sitemap(), mdx()],
-    markdown: {
-        remarkPlugins: [[remarkToc, {
-            tight: true,
-            heading: "Contents"
-        }],
-            // [remarkCollapse, {
-            //     test: "Table of contents"
-            // }]
-        ],
-        shikiConfig: {
-            theme: "one-dark-pro",
-            wrap: true
+  site: SITE.website,
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    react(),
+    sitemap(),
+    mdx(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          tight: true,
+          heading: "Contents",
         },
-        extendDefaultPlugins: true
-    },
-    vite: {
-        optimizeDeps: {
-            exclude: ["@resvg/resvg-js"]
-        }
-    },
-    // build: {
-    //     // Example: Generate `page.html` instead of `page/index.html` during build.
-    //     format: 'file'
-    // }
+      ],
+      // [remarkCollapse, {
+      //     test: "Table of contents"
+      // }]
+    ],
 
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
+  // build: {
+  //     // Example: Generate `page.html` instead of `page/index.html` during build.
+  //     format: 'file'
+  // }
 });
-
