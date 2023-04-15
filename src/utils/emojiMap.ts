5c673bc9ac9f6f0ebb86a224d2b178dbcdf38e9f
Matt <59450879+mattppal@users.noreply.github.com>
2023-04-15 07:44:04 -0400

    favicon

---
+++ src/utils/emojiMap.ts
@@ -0,0 +1,11 @@
+export const emojiMap = {
+  "🤯": "https://cdnjs.cloudflare.com/ajax/libs/twemoji/14.0.2/svg/1f92f.svg",
+  "🌶️": "",
+  "🪞": "",
+  "⚖️": "",
+  "😸": "",
+  "🤓": "",
+  "📕": "",
+  "🏹": "",
+  "😵‍💫": "",
+};
