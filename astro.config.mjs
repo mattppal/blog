e17a4bda87ace29b8119aa637a0b0e267f80ef2a
Matt <59450879+mattppal@users.noreply.github.com>
2023-04-28 07:37:15 -0400

    remove ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-// import astroRemark from '@astrojs/markdown-remark';
 
 import mdx from "@astrojs/mdx";
 
