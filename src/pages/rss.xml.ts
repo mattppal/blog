e6716dc5667b2417a93c71ac525d5699cdad99c2
Matt <59450879+mattppal@users.noreply.github.com>
2023-08-01 08:40:24 -0700

    draft

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,14 +3,6 @@ import { getCollection } from "astro:content";
 import getSortedPosts from "@utils/getSortedPosts";
 import slugify from "@utils/slugify";
 import { SITE } from "@config";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-
-const parser = new MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-});
 
 export async function get() {
   const posts = await getCollection("blog");
@@ -19,23 +11,11 @@ export async function get() {
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
-    items: sortedPosts.map(({ data, body }) => ({
+    items: sortedPosts.map(({ data }) => ({
       link: `posts/${slugify(data)}`,
-      title: sanitizeHtml(data.title),
+      title: data.title,
       description: data.description,
       pubDate: new Date(data.pubDatetime),
-      ogImage: data.ogImage,
-      customData: `<meta property="og:image" content="${data.ogImage}" />`,
-      content: sanitizeHtml(parser.render(body), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
-          "img",
-          "style",
-          "center",
-        ]),
-        exclusiveFilter: function (h2) {
-          return h2.tag == "h2" && h2.text == "ToC";
-        },
-      }),
     })),
   });
 }
