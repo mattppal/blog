c57cfa9ca8a2145a7c580809873a20ab3009caa1
Matt <59450879+mattppal@users.noreply.github.com>
2023-09-29 06:48:14 -0700

    css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -61,5 +61,5 @@ module.exports = {
     },
     // },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"),require('@tailwindcss/aspect-ratio')],
 };
