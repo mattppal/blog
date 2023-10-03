ad51d3b154a6accb9ae93d797bf6f8b6d5032c44
Matt <59450879+mattppal@users.noreply.github.com>
2023-10-03 14:18:07 -0700

    og

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  site: "https://mattpalmer.io/", // replace this with your deployed domain
   integrations: [
     tailwind({
       applyBaseStyles: false,
