e5c0f54f693bef734bdaca38f06e3c54019b94b3
Matt <59450879+mattppal@users.noreply.github.com>
2023-10-05 08:38:49 -0700

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website, // replace this with your deployed domain
+  site: SITE.website,
   integrations: [
     tailwind({
       applyBaseStyles: false,
