c1662a7373db1fabef9b39bcae10053bc985ae1c
Matt <59450879+mattppal@users.noreply.github.com>
2023-10-21 07:56:57 -0700

    og

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const SITE: Site = {
   team: "Data",
   company: "Mage",
   location: "San Jose, CA",
-  // ogImage: "og-manual.png",
+  ogImage: "og.png",
   googleAnalyticsId: "",
   lightAndDarkMode: true,
   postPerPage: 8,
