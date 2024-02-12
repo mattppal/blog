28e172c29905828f55e71ec5674dafb6732a8fd7
Matt <59450879+mattppal@users.noreply.github.com>
2024-02-12 08:02:47 -0800

    og

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ export const SITE: Site = {
   team: "DevRel",
   company: "Replit",
   location: "San Jose, CA",
-  ogImage: "/og-new.png",
+  ogImage: "/og.png",
   lightAndDarkMode: true,
   postPerPage: 8,
 };
